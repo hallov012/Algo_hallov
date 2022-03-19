147142f559236539fe4b243384b606bf6f3b4827
hyelim.mac <hallov012@gmail.com>
2022-03-20 01:09:59 +0900

    0318 pass

--- 0318/SWEA_2477_차량정비소/s1.py
+++ 0318/SWEA_2477_차량정비소/s1.py
@@ -15,19 +15,18 @@ for tc in range(1, T + 1):
     b_user = [[] for _ in range(M)]
     waiting_user = []
     end = 0
-    t = 0
     while end < K:
         # 정비 창구 처리
         for k in range(M):
-            if not b_list[k] and b_list_idx[k] != 0:
-                end += 1
-                b_list_idx[k] = 0
-            elif b_list[k] > 0:
+            if b_list[k] > 0:
                 b_list[k] -= 1
+                if not b_list[k]:
+                    end += 1
+                    b_list_idx[k] = 0
+
 
         # 기다리고 있는 고객 처리
         cnt = 0
-        waiting_user.sort(key=lambda x: x[0])
         for user in waiting_user:
             for b in range(M):
                 if not b_list[b] and not b_list_idx[b]:
@@ -43,24 +42,25 @@ for tc in range(1, T + 1):
         for j in range(N):
             if a_list[j] > 0:
                 a_list[j] -= 1
-                if not a_list[j] and a_list_idx[j] != 0:
-                    for v in range(M):
-                        if not b_list[v] and b_list_idx[v] != 0:
-                            if not waiting_user:
+                if not a_list[j]:
+                    if 0 in b_list:
+                        for v in range(M):
+                            if not b_list[v]:
                                 b_list[v] = b_time[v]
+                                b_list_idx[v] = a_list_idx[j]
                                 b_user[v].append(a_list_idx[j])
                                 a_list_idx[j] = 0
                                 break
-                        else:
-                            waiting_user.append([j, a_list_idx[j]])
-                            a_list_idx[j] = 0
+                    else:
+                        waiting_user.append([j, a_list_idx[j]])
+                        a_list_idx[j] = 0
 
         # 도착한 시간 순으로 접수 창고에 넣는 처리
         for i in range(K):
             if not arrive_time[i]:
                 for a in range(N):
                     if not a_list[a] and not a_list_idx[a]:
-                        a_list[a] = a_time[a] + 1
+                        a_list[a] = a_time[a]
                         a_list_idx[a] = i+1
                         a_user[a].append(i+1)
                         arrive_time[i] = -1
@@ -68,13 +68,11 @@ for tc in range(1, T + 1):
             elif arrive_time[i] > 0:
                 arrive_time[i] -= 1
 
-    print(a_user, b_user)
-
     ans_a = a_user[A - 1]
     ans_b = b_user[B - 1]
     intersection = list(set(ans_a) & set(ans_b))
     ans = 0
-    if len(intersection) == 1:
+    if not intersection:
         ans = -1
     else:
         for v in intersection:
