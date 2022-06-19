e5b4ef38a6bfe47474f130f3d52a5cf3fbc49d5e
Hyelim <hallov012@gmail.com>
2022-06-19 19:04:39 +0900

    0618

---
+++ 2022.06/0618/BOJ_1912_연속합/s1.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+nums = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = nums[0]
+for i in range(1, n):
+    dp[i] = max(nums[i], dp[i-1]+nums[i])
+ans = max(dp)
+print(ans)
