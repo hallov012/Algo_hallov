f8c909915a3ccf67e303ed1dcfcfd2315367d570
Hyelim <hallov012@gmail.com>
2022-11-07 22:59:17 +0900

    1106

---
+++ 2022.11/1106/BOJ_15661_링크와스타트/s1.py
@@ -0,0 +1,42 @@
+import sys
+sys.stdin = open('input.txt')
+
+def count(lst):
+    cnt = 0
+    for x in lst:
+        for y in lst:
+            cnt += arr[x][y]
+    return cnt
+
+def dfs(cnt, p, idx):
+    global ans
+    if cnt == p:
+        link = []
+        start = []
+        for i in range(1, n+1):
+            if visited[i]:
+                link.append(i)
+            else:
+                start.append(i)
+        temp1 = count(link)
+        temp2 = count(start)
+        ans = min(ans, abs(temp1-temp2))
+        return
+    for i in range(idx+1, n+1):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(cnt+1, p, i)
+            visited[i] = 0
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = [[0] * (n+1)]
+for _ in range(n):
+    line = [0] + list(map(int, input().split()))
+    arr.append(line)
+ans = sys.maxsize
+for i in range(1, n):
+    visited = [0] * (n+1)
+    dfs(0, i, 0)
+print(ans)
--- 보류/BOJ_25682_체스판다시칠하기2/s1.py
+++ 보류/BOJ_25682_체스판다시칠하기2/s1.py
@@ -1,6 +1,37 @@
 import sys
 sys.stdin = open('input.txt')
 
+input = sys.stdin.readline
+
 n, m, k = map(int, input().split())
 arr = [input().rstrip() for _ in range(n)]
-print(arr)
+bw_arr = [[0] * (m+1) for _ in range(n+1)]
+odd_line = "BW" * (m//2)
+even_line = "WB" * (m//2)
+
+if m % 2:
+    odd_line += "B"
+    even_line += "W"
+
+for i in range(n):
+    # WBWBWB 순서
+    if i % 2:
+        for j in range(m):
+            if arr[i][j] != even_line[j]:
+                bw_arr[i+1][j+1] += 1
+            bw_arr[i+1][j+1] += bw_arr[i+1][j]
+    else:
+        for j in range(m):
+            if arr[i][j] != odd_line[j]:
+                bw_arr[i+1][j+1] += 1
+            bw_arr[i+1][j+1] += bw_arr[i+1][j]
+    bw_arr[i+1][j+1] += bw_arr[i][j+1]
+
+print(bw_arr)
+
+ans = sys.maxsize
+for i in range(n-k+1):
+    for j in range(m-k+1):
+        temp = bw_arr[i+k][j+k] - bw_arr[i][j]
+        ans = min(ans, temp, k*k-temp)
+print(ans)
