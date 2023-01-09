b040f29db87384aa28778a896175dc2334c06d18
Hyelim <hallov012@gmail.com>
2023-01-10 01:02:17 +0900

    0109

--- 2023.01/BOJ_9019_DSLR/s1.py
+++ 2023.01/BOJ_9019_DSLR/s1.py
@@ -2,19 +2,32 @@ import sys
 from collections import deque
 sys.stdin = open('input.txt')
 
+input = sys.stdin.readline
+
 T = int(input())
 
 for _ in range(T):
-    a, b = map(int, input().split())
+    start, end = map(int, input().split())
     n = 10000
     visited = [0] * n
-    visited[a] = 1
-    que = deque([(a, 1, "")])
+    visited[start] = "."
+    que = deque([start])
+    counter = {0: "D", 1: "S", 2: "L", 3: "R"}
     while que:
-        now, value, temp = que.popleft()
-        next = [0] * 4
-        next[0] = (now*2) % n
-        next[1] = now-1
-        if not next[1]:
-            next[1] = 9999
+        now = que.popleft()
+        if now == end:
+            print(visited[now][1:])
+            break
 
+        d = (now * 2) % n
+        s = (now + 9999) % n
+        first = now // 1000
+        last = now % 10
+        l = (now % 1000) * 10 + first
+        r = now // 10 + 1000 * last
+        dslr = [d, s, l, r]
+        for i in range(4):
+            num = dslr[i]
+            if not visited[num]:
+                visited[num] = visited[now] + counter[i]
+                que.append(num)
