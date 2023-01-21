7c021819e717e816c804883798d7b35999f33642
hailey <hallov012@gmail.com>
2023-01-21 18:58:06 +0900

    0120

--- 2023.01/BOJ_21610_마법사상어와비바라기/s1.py
+++ 2023.01/BOJ_21610_마법사상어와비바라기/s1.py
@@ -8,14 +8,38 @@ move = [list(map(int, input().split())) for _ in range(m)]
 cloud = [[0] * n for _ in range(n)]
 dx = [0, -1, -1, -1, 0, 1, 1, 1]
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-que = [(n-1, 0), (n-1, 1), (n-2, 0), (n-2, 1)]
-for x, y in cloud:
+stack = [(n-1, 0), (n-1, 1), (n-2, 0), (n-2, 1)]
+for x, y in stack:
     cloud[x][y] = 1
+
 for d, s in move:
     d -= 1
-    new_que = []
-    for x, y in que:
+    move_stack = []
+    for x, y in stack:
         nx = ((x + dx[d]) * s) % n
         ny = ((y + dy[d]) * s) % n
         arr[nx][ny] += 1
-        for
+        move_stack.append((nx, ny))
+
+    for x, y in move_stack:
+        cnt = 0
+        for i in (-1, 1):
+            for j in (-1, 1):
+                nx = x + i
+                ny = y + j
+                if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] > 0:
+                    cnt += 1
+        arr[x][y] += cnt
+
+    new_stack = []
+    for x in range(n):
+        for y in range(n):
+            if (x, y) not in move_stack and arr[x][y] >= 2:
+                arr[x][y] -= 2
+                new_stack.append((x, y))
+    stack = new_stack
+
+ans = 0
+for row in arr:
+    ans += sum(row)
+print(ans)
