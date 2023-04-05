04f231ae92fd14f7c52eff08b12c2f6d26bf1728
hailey <hallov012@gmail.com>
2023-04-05 20:09:44 +0900

    0404

--- 2023.04/BOJ_3055_탈출/s1.py
+++ 2023.04/BOJ_3055_탈출/s1.py
@@ -9,17 +9,37 @@ def water_move():
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < r and 0 <= ny < c and (arr[nx][ny] == '.' or arr[nx][ny] == 'S'):
+            if 0 <= nx < r and 0 <= ny < c and arr[nx][ny] == '.':
                 arr[nx][ny] = '*'
                 new_water.append((nx, ny))
     return new_water
 
+def move():
+    new_que = deque()
+    while que:
+        x, y = que.popleft()
+        if arr[x][y] == 'D':
+            print(visited[x][y] - 1)
+            exit()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c and not visited[nx][ny]:
+                if arr[nx][ny] == '.' or arr[nx][ny] == 'D':
+                    visited[nx][ny] = visited[x][y] + 1
+                    new_que.append((nx, ny))
+    if not new_que:
+        print("KAKTUS")
+        exit()
+    return new_que
+
+input = sys.stdin.readline
 
 r, c = map(int, input().split())
 arr = [list(input()) for _ in range(r)]
-print(arr)
 water = deque()
 que = deque()
+visited = [[0] * c for _ in range(r)]
 sx, sy = 0, 0
 ex, ey = 0, 0
 dx = [1, -1, 0, 0]
@@ -30,9 +50,12 @@ for i in range(r):
             water.append((i, j))
         elif arr[i][j] == 'S':
             sx, sy = i, j
-        else:
-            ex, ey = i, j
             arr[i][j] = '*'
+            visited[i][j] = 1
+        elif arr[i][j] == 'D':
+            ex, ey = i, j
+que.append((sx, sy))
 
 while True:
     water = water_move()
+    que = move()
