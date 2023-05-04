23a0fdd31f4a3dd85f0401ede7479849afa38ef1
hailey <hallov012@gmail.com>
2023-05-04 19:49:15 +0900

    0503

--- 2023.05/BOJ_14595_동방프로젝트Large/s1.py
+++ 2023.05/BOJ_14595_동방프로젝트Large/s1.py
@@ -1,6 +1,8 @@
 import sys
 sys.stdin = open('input.txt')
 
+input = sys.stdin.readline
+
 def find(x):
     if p[x] != x:
         p[x] = find(p[x])
@@ -14,3 +16,11 @@ def union(x, y):
 n = int(input())
 m = int(input())
 p = list(range(n+1))
+for _ in range(m):
+    a, b = map(int, input().split())
+    while find(a) != find(b):
+        n -= 1
+        c = find(a) + 1
+        union(a, b)
+        a = c
+print(n)
