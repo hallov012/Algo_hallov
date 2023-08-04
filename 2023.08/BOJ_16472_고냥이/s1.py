370d5e27c30625a96410d22280947cc0f266703a
hyelim <hallov012@gmail.com>
2023-08-04 19:49:16 +0900

    0803

--- 2023.08/BOJ_16472_고냥이/s1.py
+++ 2023.08/BOJ_16472_고냥이/s1.py
@@ -3,30 +3,27 @@ sys.stdin = open('input.txt')
 
 n = int(input())
 word = input()
-if len(word) == 1:
-    print(1)
-    exit()
-elif len(set(word)) <= n:
+if len(set(word)) <= n:
     print(len(word))
     exit()
 
 left, right = 0, 1
-part = set()
-part.add(word[0])
+temp = set()
+temp.add(word[0])
 ans = 0
-while left < len(word)-1:
+while left <= right:
     if right == len(word):
-        ans = max(ans, right-left)
+        ans = max(ans, right - left)
         break
-    part.add(word[right])
-    if len(part) == n:
-        ans = max(ans, right-left+1)
+    temp.add(word[right])
+    if len(temp) == n:
+        ans = max(ans, right - left + 1)
         right += 1
-    elif len(part) > n:
-        part.clear()
+    elif len(temp) > n:
+        temp = set()
         left += 1
-        part.add(word[left])
         right = left + 1
+        temp.add(word[left])
     else:
         right += 1
 
