82328a3bf7edc676db92cf9a9516d466b7f5d6ac
hyelim <hallov012@gmail.com>
2023-10-09 15:57:39 +0900

    1008

--- 2023.10/BOJ_17086_아기상어2/s1.py
+++ 2023.10/BOJ_17086_아기상어2/s1.py
@@ -2,14 +2,29 @@ import sys
 from collections import deque
 sys.stdin = open('input.txt')
 
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+que = deque()
+arr = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    arr.append(row)
+    for j in range(m):
+        if row[j]:
+            que.append((i, j))
+
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 ans = 0
-for i in range(n):
-    for j in range(m):
-        if arr[i][j]:
-            visited = [[False] * m for _ in range(n)]
-            visited[i][j] = 1
-            que =
+while que:
+    x, y = que.popleft()
+    for d in range(8):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < n and 0 <= ny < m:
+            if not arr[nx][ny]:
+                que.append((nx, ny))
+                arr[nx][ny] = arr[x][y] + 1
+                ans = max(ans, arr[nx][ny])
+print(ans-1)
