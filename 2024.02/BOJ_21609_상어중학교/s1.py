19c13ddba22621fe04d33436e0cb06e2095f898d
hyelim <hallov012@gmail.com>
2024-02-17 22:08:42 +0900

    0216

--- 2024.02/BOJ_21609_상어중학교/s1.py
+++ 2024.02/BOJ_21609_상어중학교/s1.py
@@ -4,36 +4,64 @@ sys.stdin = open('input.txt')
 
 def find_group():
     block_list = []
+    idx = 1
     for i in range(n):
         for j in range(n):
             if arr[i][j] > 0 and not visited[i][j]:
-                cnt, block = bfs(i, j)
-                heapq.heappush(block_list, (-cnt, block))
+                block, r_block = bfs(i, j, idx)
+                c_cnt, r_cnt = len(block), len(r_block)
+                heapq.heappush(block_list, (-(c_cnt + r_cnt), -r_cnt, block, r_block))
+                idx += 1
     return block_list
 
-
-def bfs(a, b):
+def bfs(a, b, idx):
     c = arr[a][b]
-    visited[a][b] = 1
+    visited[a][b] = idx
     que = deque([(a, b)])
-    block = [(a, b)]
-    cnt = 1
-    r_cnt = 0
+    block = [(-a, -b)]
+    r_block = []
     while que:
         x, y = que.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n:
-                # 무지개는 여기저기 사용될 수 있긴 한데, 한번의 시도에서는 한번만 사용되는거니까 그거 visited 체크 해야함
-                # 몇번째 블록인지 idx를 받아서 그걸로 예외처리 하는 방법을 생각해보자
-                if (arr[nx][ny] == c and not visited[nx][ny]) or arr[nx][ny] == 0:
-                    visited[nx][ny] = 1
+                if (arr[nx][ny] == c or arr[nx][ny] == 0) and visited[nx][ny] != idx:
+                    visited[nx][ny] = idx
                     que.append((nx, ny))
-                    block.append((nx, ny))
                     if arr[nx][ny] == 0:
-                        r_cnt += 1
-    return cnt, block
+                        r_block.append((nx, ny))
+                    else:
+                        heapq.heappush(block, (-nx, -ny))
+
+    return block, r_block
+
+def gravity():
+    for j in range(n):
+        for i in range(n-1, -1, -1):
+            if arr[i][j] != empty and arr[i][j] != -1:
+                idx = i+1
+                while idx < n and arr[idx][j] == empty:
+                    idx += 1
+                if idx-1 != i:
+                    arr[idx-1][j] = arr[i][j]
+                    arr[i][j] = empty
+
+def rotate_board():
+    new_arr = [[-2] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            new_arr[n-j-1][i] = arr[i][j]
+    return new_arr
+
+def check_game_end():
+    if not blocks:
+        return True
+    if blocks[0][0] > -2:
+        return True
+    return False
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
@@ -46,12 +74,29 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
+empty = -2
+ans = 0
 
 while True:
     visited = [[0] * n for _ in range(n)]
     blocks = find_group()
     print(blocks)
-    break
+    if check_game_end():
+        break
+
+    cnt, location, r_location = blocks[0][0], blocks[0][2], blocks[0][3]
+    ans += cnt ** 2
+    for x, y in location:
+        arr[-x][-y] = empty
+    for x, y in r_location:
+        arr[x][y] = empty
+    gravity()
+    arr = rotate_board()
+    gravity()
+
+print(ans)
+
+
 
 
 
