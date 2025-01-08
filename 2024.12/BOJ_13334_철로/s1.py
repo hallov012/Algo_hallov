a19ab3aa8f9e8dda15d74240ef7f3e270fd68fda
hyelim <hallov012@gmail.com>
2025-01-08 23:57:34 +0900

    1214

---
+++ 2024.12/BOJ_13334_철로/s1.py
@@ -0,0 +1,20 @@
+import sys, heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+employees = [sorted(list(map(int, input().split()))) for _ in range(n)]
+employees.sort(key=lambda x: (x[1], x[0]))
+d = int(input())
+
+que = []
+ans = 0
+
+for s, e in employees:
+    heapq.heappush(que, s)
+    d_start = e - d
+    while que and que[0] < d_start:
+        heapq.heappop(que)
+    ans = max(ans, len(que))
+
+print(ans)
--- 2024.12/BOJ_1799_비숍/s1.py
+++ 2024.12/BOJ_1799_비숍/s1.py
@@ -2,3 +2,5 @@ import sys
 sys.stdin = open('input.txt')
 
 n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
