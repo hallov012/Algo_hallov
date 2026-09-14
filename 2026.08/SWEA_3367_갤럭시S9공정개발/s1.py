1c69a9ea678b8a1553ac73d489ce65fd169be1e1
hyelim <hallov012@gmail.com>
2026-09-14 23:03:01 +0900

    0826

--- 2026.08/SWEA_3367_갤럭시S9공정개발/s1.py
+++ 2026.08/SWEA_3367_갤럭시S9공정개발/s1.py
@@ -2,12 +2,27 @@ import sys
 sys.stdin = open('input.txt')
 
 T = int(input())
+INF = 10 ** 30
 for tc in range(1, T+1):
     k = int(input())
-    files = map(int, input().split())
+    files = list(map(int, input().split()))
 
-    add_sum = [0] * (k+1)
-    for i in range(k):
-        add_sum[i+1] = add_sum[i] + files[i]
+    arr = [INF] + files + [INF]
+    ans = 0
 
-    dp = []
+    while len(arr) > 3:
+        i = 1
+        # arr[i-1] <= arr[i+1] 을 만족하는 위치 탐색
+        while arr[i-1] > arr[i+1]:
+            i += 1
+        # i-1, i 합치기
+        merged = arr[i-1] + arr[i]
+        ans += merged
+
+        del arr[i-1:i+1]
+        j = i-2
+        while arr[j] < merged:
+            j -= 1
+        arr.insert(j+1, merged)
+
+    print(f"#{tc} {ans}")
